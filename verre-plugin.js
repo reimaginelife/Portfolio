354cd5e5b1ee02d649b04f7de4a531537417d967
Raymond <reimagineus@gmail.com>
2019-04-14 05:35:28 -0700

    Improved Transition

--- verre-plugin.js
+++ verre-plugin.js
@@ -1,9 +1,9 @@
 function myFunction() {
   var x = document.getElementById("popup");
-  if (x.style.opacity !== "1") {
-    x.style.opacity= "1";
-    x.style.zIndex= "100";
-    /*x.style.transition = "all 2s";*/
+  if (x.style.visibility !== "visible") {
+    x.style.opacity= "visible";
+    /*x.style.zIndex= "100";*/
+
   } else {
     x.style.opacity = "";
     x.style.zIndex= "";
