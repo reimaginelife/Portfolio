ce2380d8517200e2839cbf52a503b9d4f1da7884
Raymond <reimagineus@gmail.com>
2019-04-18 12:33:30 -0700

    Updated code

---
+++ verre-plugin.js
@@ -0,0 +1,12 @@
+function myFunction() {
+  var x = document.getElementById("popup");
+  if (x.style.opacity !== "1") {
+    x.style.opacity= "1";
+    x.style.zIndex= "100";
+
+  } else {
+    x.style.opacity = "";
+    x.style.zIndex= "";
+  }
+
+}
